83da8c0b98d286d91478bcdf0f0a087b3f7a7663
Mahi Sawner <mahisawner1234@gmail.com>
2026-02-11 23:33:53 +0530

    react setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const process = require('process')
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the backend!' })
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-600">
+        Tailwind is Working 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
