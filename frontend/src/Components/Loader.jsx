abf4207d45bc48f88060bde4822347b2115ee8e6
Mahi Sawner <mahisawner1234@gmail.com>
2026-02-14 22:52:34 +0530

    Loader.jsx

---
+++ frontend/src/Components/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="flex justify-center items-center h-60">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-green-500 rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loader;
