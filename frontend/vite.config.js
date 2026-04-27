1d3a4d9505e95d06eecb5d356a69885e5fa27162
Mahi Sawner <mahisawner1234@gmail.com>
2026-04-27 17:10:25 +0530

    added pm2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 5173,
-    host: true,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5001',
+        target: 'http://backend:5001',
         changeOrigin: true,
       },
     },
   },
-  preview: {
-    port: 5173,
-    host: true,
-  }
 })
